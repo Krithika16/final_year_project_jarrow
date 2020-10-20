98ae8d3433008d2d4b475017d7645b5ee3aa6d35
Joearrowsmith <joearrowsmith98@gmail.com>
2020-10-20 23:15:52 +0100

    Moved to custom train loop and generator

--- Auto_Augment/core/mnist.py
+++ Auto_Augment/core/mnist.py
@@ -1,9 +1,10 @@
 from Auto_Augment.core.util import set_memory_growth
-from Auto_Augment.core.util.augmentation_2d import apply_random_gamma
+from Auto_Augment.core.util.augmentation_2d import *
 import tensorflow as tf
 import numpy as np
 import random
 
+import matplotlib.pyplot as plt
 
 def get_mnist():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
@@ -12,21 +13,37 @@ def get_mnist():
     x_train, y_train = x_train[:-val_length, ...], y_train[:-val_length, ...]
     x_val, y_val = x_train[-val_length:, ...], y_train[-val_length:, ...]
 
-    train = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    val = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-    test = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    x_train = np.float32(x_train)
+    x_val = np.float32(x_val)
+    x_test = np.float32(x_test)
 
+    train = (x_train, y_train)
+    val = (x_val, y_val)
+    test = (x_test, y_test)
     return train, test, val
+
+
+def data_generator(x, y, batch_size=32, train=True):
+    x_len = len(x)
+    idxes = np.array(list(range(x_len)))
+    np.random.shuffle(idxes)
+    idx = 0
+    while idx + batch_size < (x_len - 1):
+        x_ = x[idx: idx+batch_size]
+        y_ = y[idx: idx+batch_size]
+        yield (x_, y_)
+        idx += batch_size
 
 
 class ConvClassifier(tf.keras.Model):
     def __init__(self):
         super(ConvClassifier, self).__init__()
         self.model_layers = [
+            tf.keras.layers.InputLayer(input_shape=(28, 28, 1), batch_size=32),
+            tf.keras.layers.Conv2D(8, (3, 3), padding='same'),
+            tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1), padding='valid'),
             tf.keras.layers.Conv2D(8, (3, 3), padding='same'),
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(128,activation='relu'),
-            tf.keras.layers.Dropout(0.1),
             tf.keras.layers.Dense(64,activation='relu'),
             tf.keras.layers.Dropout(0.1),
             tf.keras.layers.Dense(10, activation='softmax')
@@ -34,11 +51,7 @@ class ConvClassifier(tf.keras.Model):
     
     def call(self, x, training=False):
         for l in self.model_layers:
-            if training:
-                x = l(x)
-            else:
-                if type(l) is not tf.keras.layers.Dropout:
-                    x = l(x)
+            x = l(x)
         return x
 
 
@@ -52,23 +65,70 @@ def get_and_compile_model(model_func):
     return model
 
 
+@tf.function
+def train_step(model, inputs, targets, optimizer, loss_func):
+    with tf.GradientTape() as tape:
+        pred = model(inputs, training=True)
+        loss = loss_func(targets, pred)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss, pred
+
+
+@tf.function
+def val_step(model, inputs, targets, loss_func):
+    pred = model(inputs, training=False)
+    loss = loss_func(targets, pred)
+    return loss, pred
+
+
+def supervised_train_loop(model, train, val, batch_size=32, epochs=20):
+    train_loss_results = []
+    train_val_loss_results = []
+    train_acc_results = []
+    train_val_acc_results = []
+
+    train_ds = tf.data.Dataset.from_generator(data_generator, (tf.float32, tf.int32), args=(*train, batch_size, True))
+    val_ds = tf.data.Dataset.from_generator(data_generator, (tf.float32, tf.int32), args=(*val, batch_size, False))
+
+    optimizer = model.optimizer
+    loss = model.loss
+
+    for e in range(epochs):
+        epoch_loss_avg = tf.keras.metrics.Mean()
+        epoch_val_loss_avg = tf.keras.metrics.Mean()
+        epoch_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+        epoch_val_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+        tf.print(f"{e+1:03d}/{epochs:03d}: ", end="")
+
+        for x, y in train_ds:
+            x, y = augmentation_policy(x, y)
+            tr_loss, tr_pred = train_step(model, x, y, optimizer, loss)
+            epoch_loss_avg.update_state(tr_loss)
+            epoch_acc.update_state(y, tr_pred)
+
+        for x, y in val_ds:
+            val_loss, val_pred = val_step(model, x, y, loss)
+            epoch_val_loss_avg.update_state(val_loss)
+            epoch_val_acc.update_state(y, val_pred)
+
+        train_loss_results.append(epoch_loss_avg.result())
+        train_val_loss_results.append(epoch_val_loss_avg.result())
+        train_acc_results.append(epoch_acc.result())
+        train_val_acc_results.append(epoch_val_acc.result())
+
+        tf.print(f"Loss: {train_loss_results[-1]:.3f}, Val Loss: {train_val_loss_results[-1]:.3f}, Acc: {train_acc_results[-1]:.3f}, Val Acc: {train_val_acc_results[-1]:.3f}")
+
+
+def augmentation_policy(x, y):
+    x, y = apply_random_left_right_flip(x, y)
+    x, y = apply_random_up_down_flip(x, y)
+    x, y = apply_random_brightness(x, y)
+    x, y = apply_random_contrast(x, y)
+    return x, y
+
+
 if __name__ == "__main__":
-    batch_size = 32
-    epochs = 2
-
     train, val, test = get_mnist()
-
-    train = train.batch(batch_size)
-    val = val.batch(batch_size)
-    test = test.batch(batch_size)
-
-    train = train.map(apply_random_gamma)
-
     model = get_and_compile_model(ConvClassifier)
-
-    hist = model.fit(
-        train,
-        epochs=epochs,
-        validation_data=val,
-    )
-    val_loss = hist.history['val_loss']
+    supervised_train_loop(model, train, val, 32, 20)
--- Auto_Augment/core/util/augmentation_2d.py
+++ Auto_Augment/core/util/augmentation_2d.py
@@ -1,9 +1,31 @@
 import tensorflow as tf
 
+def apply_random_brightness(image_tensor, label_tensor, do_prob=1.0, magnitude=1.0):
+    if tf.random.uniform(()) <= do_prob:
+        image_tensor = tf.image.random_brightness(image_tensor, magnitude)
+    return image_tensor, label_tensor
 
-def apply_random_gamma(image_tensor, label_tensor):
-    do_gamma = tf.random.uniform([]) > 0.75
-    gamma = tf.random.uniform([], 0.9, 1.1)
-    gain = tf.random.uniform([], 0.95, 1.05)
-    image_tensor = tf.cond(do_gamma, lambda: tf.image.adjust_gamma(image_tensor, gamma=gamma, gain=gain), lambda: image_tensor)
+def apply_random_left_right_flip(image_tensor, label_tensor, do_prob=1.0, magnitude=None):
+    if tf.random.uniform(()) <= do_prob:
+        image_tensor = tf.image.random_flip_left_right(image_tensor, magnitude)
+    return image_tensor, label_tensor
+
+def apply_random_up_down_flip(image_tensor, label_tensor, do_prob=1.0, magnitude=None):
+    if tf.random.uniform(()) <= do_prob:
+        image_tensor = tf.image.random_flip_up_down(image_tensor, magnitude)
+    return image_tensor, label_tensor
+
+def apply_random_shear(image_tensor, label_tensor, do_prob=1.0, magnitude=20.0):
+    if tf.random.uniform(()) <= do_prob:
+        image_tensor = tf.keras.preprocessing.image.random_shear(image_tensor, magnitude),
+    return image_tensor, label_tensor
+
+def apply_random_zoom(image_tensor, label_tensor, do_prob=1.0, magnitude=None):
+    # if tf.random.uniform(()) <= do_prob:
+    image_tensor = tf.image.random_crop(image_tensor, 0.9)
+    return image_tensor, label_tensor
+
+def apply_random_contrast(image_tensor, label_tensor, do_prob=1.0, magnitude=None):
+    if tf.random.uniform(()) <= do_prob:
+        image_tensor = tf.image.random_contrast(image_tensor, 0.0, 1.0)
     return image_tensor, label_tensor
