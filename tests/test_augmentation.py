728caa717a09bced4b4b9e9f42131159b1199517
JoeArrowsmith <joearrowsmith98@gmail.com>
2021-03-03 16:32:53 +0000

    added aug tests

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -1,6 +1,6 @@
 import pytest
 
-
+@pytest.mark.skip
 @pytest.mark.parametrize(
     "e_total, aug_apps, expected_output",
     [
@@ -34,3 +34,64 @@ def test_half_aug_policy_idxes(e_total, aug_apps, expected_output):
     idxes = pol.get_interval_idxes(e_total, aug_apps)
     print(idxes, "==", expected_output)
     assert idxes == expected_output
+
+
+@pytest.mark.skip
+def test_augs_import():
+    import augpolicies.augmentation_funcs.augmentation_1d
+    import augpolicies.augmentation_funcs.augmentation_2d
+
+
+@pytest.mark.parametrize("func",['apply_random_left_right_flip', 'apply_random_up_down_flip'])
+@pytest.mark.parametrize('batch', [True])
+@pytest.mark.parametrize('apply_to_y', [True, False])
+@pytest.mark.parametrize('c', [None, 1, 3])
+def test_random_aug_func_with_apply_to_y(func, batch, apply_to_y, c):
+    import importlib
+    import tensorflow as tf
+    import numpy as np
+    m = importlib.import_module(f'augpolicies.augmentation_funcs.augmentation_2d')
+    func = getattr(m, func)
+    (x_train, _), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+    r = 10 if apply_to_y else 1
+    for _ in range(r):
+        if batch:
+            data = x_train[:64]
+        else:
+            data = x_train[0]
+        if c:
+            data = data[..., np.newaxis]
+            if c == 3:
+                target_shape = list(data.shape)
+                target_shape[-1] = c
+                data = np.broadcast_to(data, target_shape)
+        trans, l = func(data, data, apply_to_y=apply_to_y)
+        assert data.shape == trans.shape
+        if apply_to_y:
+            assert trans.shape == l.shape
+            assert np.array_equal(trans, l)
+
+
+@pytest.mark.parametrize("func",['apply_random_brightness', 'apply_random_contrast'])
+@pytest.mark.parametrize('batch', [True])
+@pytest.mark.parametrize('c', [None, 1, 3])
+def test_random_aug_func(func, batch, c):
+    import importlib
+    import tensorflow as tf
+    import numpy as np
+    m = importlib.import_module(f'augpolicies.augmentation_funcs.augmentation_2d')
+    func = getattr(m, func)
+
+    (x_train, _), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+    if batch:
+        data = x_train[:32]
+    else:
+        data = x_train[0]
+    if c:
+        data = data[..., np.newaxis]
+        if c == 3:
+            target_shape = list(data.shape)
+            target_shape[-1] = c
+            data = np.broadcast_to(data, target_shape)
+    trans, _ = func(data, data)
+    assert data.shape == trans.shape
