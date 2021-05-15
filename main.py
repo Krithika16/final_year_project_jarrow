7c2b1dae0be6fb839768db6e2c1cade6b040a6e8
joearrowsmith <joearrowsmith98@gmail.com>
2021-05-15 22:15:31 +0100

    adding basic logging

--- main.py
+++ main.py
@@ -8,19 +8,27 @@ logger = get_logger(args)
 try:
     if args.hpc:
         if args.rank:
+            logger.info("starting aug_comparison_ranking...")
             import augpolicies.hypothesis_testing.aug_comparison_ranking
         elif args.vis:
+            logger.info("starting aug_comparison_analysis...")
             import augpolicies.hypothesis_testing.aug_comparison_analysis
         else:
+            logger.info("starting aug_comparison...")
             import augpolicies.hypothesis_testing.aug_comparison
     elif args.hpe:
         # if args.rank:
         #     import augpolicies.hypothesis_testing.augmentation_at_end_ranking
         if args.vis:
+            logger.info("starting augmentation_at_end_analysis...")
             import augpolicies.hypothesis_testing.augmentation_at_end_analysis
         else:
+            logger.info("starting augmentation_at_end...")
             import augpolicies.hypothesis_testing.augmentation_at_end
 except (SystemExit, KeyboardInterrupt):
+    logger.info("SystemExit or KeyboardInterrupt.")
     raise
 except Exception as exception:
-    logger.error('Failed to open file', exc_info=True)
+    logger.error('Exception:', exc_info=True)
+
+logger.info("Complete.")
